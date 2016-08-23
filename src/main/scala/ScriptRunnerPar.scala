a5e4de4fdced8032ab58b28ae0ef1b09d7a1d364
Brandon Elam Barker <beb82@cornell.edu>
2016-08-23 10:07:55 -0400

    finished first parallel execution example

--- src/main/scala/ScriptRunnerPar.scala
+++ src/main/scala/ScriptRunnerPar.scala
@@ -1,8 +1,9 @@
 import edu.cornell.cac.docker.api._
 import edu.cornell.cac.docker.api.entities.{ContainerConfig, ContainerId, RepositoryTag}
 
-import scala.concurrent.Future
-import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Future, TimeoutException}
+import scala.concurrent.duration.{Duration, MINUTES, SECONDS}
+import scala.concurrent.ExecutionContext.Implicits.global
 //import edu.cornell.cac.docker.api.json.Formats._        // use this for API version < v1.12
 import edu.cornell.cac.docker.api.json.FormatsV112._
 
@@ -16,28 +17,61 @@ object ScriptRunnerPar {
     implicit val docker = Docker("localhost")
     val timeout = Duration(30, SECONDS)
     val containerCount = 4
+    val runtime = Duration(20, SECONDS)
 
     val cmd = Seq("/bin/sh", "-c", "while true; do echo hello world; sleep 1; done")
     val containerName = "reactive-docker"
     val imageTag = RepositoryTag.create("busybox", Some("latest"))
     val cfg = ContainerConfig("busybox", cmd)
 
-
     // create image, returns a list of docker messages when finished
     val messages = Await.result(docker.imageCreate(imageTag), timeout)
 
+    //TODO: maybe better to create in parallel?
     // create containers
     val futurecContainerIds: Seq[Future[ContainerId]] = (1 to containerCount).map{ii =>
-      docker.containerCreate("busybox", cfg, Some(containerName))
+      docker.containerCreate("busybox", cfg, Some(containerName + ii.toString))
     }.map{fut => fut.map{case(futId, msgs) => futId}}
 
     // run containers
-    Future{
-      futurecContainerIds.foreach{fId =>
-        val containerId = Await.result(fId, timeout)
-        Await.result(docker.containerStart(containerId), timeout)
-        println(s"container $containerId is running")
-      }
+    val initiations = futurecContainerIds.map{fId =>
+      val containerId = Await.result(fId, timeout)
+      println(s"starting container $containerId")
+      docker.containerStart(containerId)
     }
+
+    Await.result(Future.sequence(initiations), Duration.Inf)
+    println("All containers have started.")
+
+    // stop & remove containers
+    val completions = futurecContainerIds.map { fId => Future {
+      val containerId = Await.result(fId, timeout)
+      Thread.sleep(runtime.toMillis)
+      try {
+        Await.result(docker.containerStop(containerId), timeout)
+        println(s"container $containerId stopped")
+      } catch {
+        case ex: TimeoutException =>
+          println(s"Stopping container $containerId timed out: ${ex.getMessage}")
+          println(s"Attempting to kill container $containerId.")
+          try {
+            Await.result(docker.containerKill(containerId), timeout)
+          } catch {
+            case ex: TimeoutException => println(s"Killing container $containerId timed out.")
+          }
+      }
+      try {
+        Await.result(docker.containerRemove(containerId), timeout)
+        println(s"container $containerId removed")
+      } catch {
+        case ex: TimeoutException =>
+          println(s"Removing container $containerId timed out: ${ex.getMessage}")
+      }
+    }}
+
+
+    Await.result(Future.sequence(completions), Duration.Inf)
+    println("All containers should be finished, terminating job.")
+
   }
 }
