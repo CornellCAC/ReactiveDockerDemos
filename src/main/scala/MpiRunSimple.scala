d9df55f09c7fea96e331bbe0e031c27066c335ff
Brandon Elam Barker <beb82@cornell.edu>
2016-09-20 16:54:26 -0400

    added initial MPI demo

--- src/main/scala/MpiRunSimple.scala
+++ src/main/scala/MpiRunSimple.scala
@@ -1,16 +1,16 @@
 import edu.cornell.cac.docker.api._
 import edu.cornell.cac.docker.api.entities.{ContainerConfig, ContainerId, RepositoryTag}
 
-import scala.concurrent.{Future, TimeoutException}
-import scala.concurrent.duration.{Duration, MINUTES, SECONDS}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, SECONDS, MINUTES}
+import scala.concurrent.{Future, TimeoutException}
 import scala.util.Try
 //import edu.cornell.cac.docker.api.json.Formats._        // use this for API version < v1.12
 import edu.cornell.cac.docker.api.json.FormatsV112._
 
 import scala.concurrent.Await
 
-object ScriptRunnerPar {
+object MpiRunSimple {
 
 
   def main(args: Array[String]): Unit = {
@@ -18,37 +18,65 @@ object ScriptRunnerPar {
     implicit val docker = Docker("localhost")
     val timeout = Duration(30, SECONDS)
     val containerCount = 4
-    val runtime = Duration(20, SECONDS)
+    val runtime = Duration(3, MINUTES)
 
     // Note that this command loops forever; a container will stop when its
     // command exits
-    val cmd = Seq("/bin/sh", "-c", "while true; do echo hello world; sleep 1; done")
-    val containerName = "reactive-docker"
-    val imageTag = RepositoryTag.create("busybox", Some("latest"))
-    val cfg = ContainerConfig("busybox", cmd)
+    val cmd = Seq("/usr/sbin/sshd", "-D")
+    val sshStopCmd = Seq("service", "ssh", "stop") // configure this to be appropriate for your OS
+    val headImg = "dockeropenmpi_mpi_head"
+    val nodeImg = "dockeropenmpi_mpi_node"
+
+    val containerName = "dockeropenmpi"
+    val imageHeadTag = RepositoryTag.create(headImg, Some("latest"))
+    val imageNodeTag = RepositoryTag.create(nodeImg, Some("latest"))
+    val cfg = ContainerConfig("dockeropenmpi", cmd)
 
     // create image, returns a list of docker messages when finished
-    val messages = Await.result(docker.imageCreate(imageTag), timeout)
+    val messages = Await.result(docker.imageCreate(imageHeadTag), timeout) ::
+      Await.result(docker.imageCreate(imageNodeTag), timeout)
 
     //TODO: maybe better to create in parallel?
     // create containers
-    val futureContainerIds: Seq[Future[ContainerId]] = (1 to containerCount).map{ii =>
-      docker.containerCreate("busybox", cfg, Some(containerName + ii.toString))
+    val futureHeadContainerId: Future[ContainerId] =
+      docker.containerCreate(headImg, cfg, Some(containerName + "_head"))
+        .map{case(futId, msgs) => futId}
+
+    val futureNodeContainerIds: Seq[Future[ContainerId]] = (1 until containerCount).map{ii =>
+      docker.containerCreate(nodeImg, cfg, Some(containerName + "_head" + ii.toString))
     }.map{fut => fut.map{case(futId, msgs) => futId}}
 
-    // run containers
-    val initiations = futureContainerIds.map{fId =>
-      val containerId = Await.result(fId, timeout)
-      println(s"starting container $containerId")
-      docker.containerStart(containerId)
+    val containerIds = (futureHeadContainerId +: futureNodeContainerIds).map{
+      fId => Await.result(fId, timeout)
+    }
+    val initiations = containerIds.map{id =>
+      println(s"starting container $id")
+      docker.containerStart(id)
     }
 
     Await.result(Future.sequence(initiations), Duration.Inf)
     println("All containers have started.")
 
+    // Do some work
+    println("Do some work ...")
+    docker.containerExecCreate(
+      containerIds.head,
+      //Seq("dd", "if=/dev/zero", "of=/dev/null")
+      "mpirun -n 2 python /home/mpirun/mpi4py_benchmarks/all_tests.py".split(' ').toSeq
+    ).flatMap{case (execId, msgs) =>
+      println("initiating exec start!") // DEBUG
+      if (msgs.nonEmpty) msgs.foreach(msg => println(msg))
+      docker.containerExecStart(execId)
+    }
+
+    //TODO: remove this and add a wait method of some sort (once modeled as futures or tasks)
+    Thread.sleep(40000)
+
+    // Attempt normal container stop by stopping CMD process
+    containerIds.foreach{id => docker.containerExec(id, sshStopCmd)}
+
     // stop & remove containers
-    val completions = futureContainerIds.map { fId => Future {
-      val containerId = Await.result(fId, timeout)
+    val completions = containerIds.map{ containerId => Future {
       Thread.sleep(runtime.toMillis)
 
       val stoppedAndRemoved: Try[Boolean] = for {
